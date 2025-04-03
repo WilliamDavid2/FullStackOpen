a5de1e50b3b68dae5af9668ebfa2f29175e85d51
WilliamDavid2 <83736535+WilliamDavid2@users.noreply.github.com>
2025-04-03 13:19:26 -0500

    Part3_Node_Express

--- Part3/pruebaServer/index.js
+++ Part3/pruebaServer/index.js
@@ -1,79 +1,31 @@
-const express = require('express')
-const app = express()
-
-app.use(express.json())
+const http =  require('http')
 
 let notes = [
-    {
-        id: 1,
-        content : "HTML is easy",
-        important : true
+    {id : 1,
+      content : "HTML is easy",
+      important: true  
     },
-    {
-        id: 2,
-        content : "Browser can execute only JavaScript",
-        important : false
+    {id : 2,
+        content: "Browser can execute only JavaScript",
+        important: true  
     },
-    {
-    id: 3,
-    content : "GET and POST are the most important methods of HTTP protocol",
-    important : true
-}
+    { id : 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true  
+    }
 ]
 
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-    return maxId + 1
-}
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1>')
+const app = http.createServer( (request, response) => {
+    response.writeHead(200, {'Content-Type': 'application/json'})
+    response.end(JSON.stringify(notes))
 
 })
 
-app.get('/api/notes',(request, response) => {
-    response.json(notes)
-})
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if(note){
-        response.json(note)
-    }else{
-        response.status(404).end()
-    }
-  })
-
-app.delete('/api/notes/:id', (request, response) =>{
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id )
-    response.status(204).end()
-})
-
-app.post('/api/notes/', (request, response) =>{
-    const body = request.body
-
-    if(!body.content){
-        return response.status(400).json({error:"message is missing"})
-    }
-
-    const note ={
-        content : body.content,
-        important : Boolean(body.important) || false,
-        id : generateId()
-    }
-    notes = notes.concat(note)
-    
-    response.json(note)
-})
-
-
-
-
-
-
+/*const app= http.createServer((request, response) =>{
+    response.writeHead(200,{'Content-Type': 'text/plain'})
+    response.end('Hello World')
+})*/
 
 const PORT = 3001
-app.listen(PORT, () =>{
-    console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
